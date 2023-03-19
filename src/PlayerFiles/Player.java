c96df1f01587bd2f7665a9417d018ccdbdd01aa5
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-03-19 12:15:55 +0100

    added file and message to user

---
+++ src/PlayerFiles/Player.java
@@ -0,0 +1,24 @@
+package PlayerFiles;
+import java.util.Scanner;
+
+public class Player {
+
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cosa vuoi fare?...");
+		playing();
+	}
+
+	private static void playing() {
+		System.out.println("1 = img");
+		System.out.println("2 = audio");
+		System.out.println("3 = vid");
+		
+		
+		
+		
+	}
+
+}
